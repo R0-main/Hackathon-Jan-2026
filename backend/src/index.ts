6e7a4d7d53865fb0cb47d53d92586052dc7d457c
AdelFC <AdelFC@users.noreply.github.com>
2026-01-17 12:15:48 +0100

    Add waitlist feature with SQLite + landing page

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import postCvRouter from './post-cv';
+import waitlistRouter from './waitlist';
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/cv', postCvRouter);
+app.use('/api/waitlist', waitlistRouter);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello from Express Backend!');
